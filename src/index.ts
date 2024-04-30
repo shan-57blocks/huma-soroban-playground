598d0c123d57d6f3fd0996367996e11d379415c6
shan <shan@57blocks.com>
2024-04-30 15:23:58 +0800

    get underlyingToken decimals

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { setPoolSettings } from './pool';
+import { getUnderlyingToken, setPoolSettings } from './pool';
 import { approveBorrower } from './poolCredit';
 import { approveLender } from './trancheVault';
 
 (async () => {
+  await getUnderlyingToken();
   // await setPoolSettings();
   //   await approveLender();
-  await approveBorrower();
+  // await approveBorrower();
 })();
--- src/pool.ts
+++ src/pool.ts
@@ -1,4 +1,5 @@
 import { Client } from '@huma/pool';
+import { scValToNative } from '@stellar/stellar-sdk';
 
 import { Accounts, findPoolMetadata, ScValType, toScVal } from './utils/common';
 import { Network, NetworkPassphrase, PublicRpcUrl } from './utils/network';
@@ -16,8 +17,26 @@ export const getUnderlyingToken = async () => {
     rpcUrl: PublicRpcUrl.testnet
   });
   try {
-    const { result } = await pool.get_underlying_token();
-    console.log('result', result);
+    const { result: underlyingTokenAddress } =
+      await pool.get_underlying_token();
+    const symbol = await sendTransaction(
+      Accounts.poolOwner.secret(),
+      network,
+      underlyingTokenAddress,
+      'symbol'
+    );
+    const decimals = await sendTransaction(
+      Accounts.poolOwner.secret(),
+      network,
+      underlyingTokenAddress,
+      'decimals'
+    );
+    const underlyingToken = {
+      address: underlyingTokenAddress,
+      symbol: scValToNative(symbol),
+      decimals: scValToNative(decimals)
+    };
+    console.log(underlyingToken);
   } catch (e) {
     console.error('Error', e);
   }
